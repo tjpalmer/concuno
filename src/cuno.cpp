724b1d6c9e4943785e3c94f1fb2d7e9cd6ffce0a
Thomas J. Palmer <tjpalmer@tjpalmer.com>
2010-11-05 12:49:46 -0500

    Basic project structure.

---
+++ src/cuno.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <iostream>
+#include "cuncuno.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  cout << "cuno" << endl;
+}
---
+++ src/tree.cpp
@@ -0,0 +1,3 @@
+namespace cuncuno {
+
+}
