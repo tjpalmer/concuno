3a1b991e41e9fbfc56e5fde26696c806c092f135
Tom <tom@tom-mini9.(none)>
2010-11-06 16:41:45 -0500

    Many more handlers defined. Handler map. More fleshed out item struct.

--- test/stackiter/loader.cpp
+++ test/stackiter/loader.cpp
@@ -2,19 +2,59 @@
 
 namespace stackiter {
 
-void Loader::handleDestroy(stringstream* tokens) {
-  int id;
-  *tokens >> id;
+Loader::Loader() {
+  handlers["color"] = &Loader::handleColor;
+  handlers["destroy"] = &Loader::handleDestroy;
+  handlers["extent"] = &Loader::handleExtent;
+  handlers["item"] = &Loader::handleItem;
+  handlers["pos"] = &Loader::handleLocation;
+  handlers["posvel"] = &Loader::handleVelocity;
+  handlers["rot"] = &Loader::handleAngle;
+  handlers["rotvel"] = &Loader::handleAngularVelocity;
+  handlers["type"] = &Loader::handleType;
+}
+
+Item& Loader::getItem(stringstream& tokens) {
+  // TODO Any validation?
+  return state.items[indexes[handleId(tokens)]];
+}
+
+void Loader::handleAngle(stringstream& tokens) {
+  Item& item = getItem(tokens);
+  // TODO Angle is in rats. Convert to radians or not?
+  tokens >> item.angle;
+}
+
+void Loader::handleAngularVelocity(stringstream& tokens) {
+  Item& item = getItem(tokens);
+  // TODO Angular velocity is in rats. Convert to radians or not?
+  tokens >> item.angularVelocity;
+}
+
+void Loader::handleColor(stringstream& tokens) {
+  Item& item = getItem(tokens);
+  // TODO Use HSV colorspace to begin with?
+  // TODO Verify we haven't run out?
+  tokens >> item.color(0);
+  tokens >> item.color(1);
+  tokens >> item.color(2);
+  // Ignore opacity, the 4th value. It's bogus for now.
+}
+
+void Loader::handleDestroy(stringstream& tokens) {
+  int id = handleId(tokens);
   int index = indexes[id];
   if (index == -1) {
     throw "double destroy";
   }
   // Remove the destroyed item.
   indexes[id] = -1;
-  items.erase(items.begin() + index);
+  state.items.erase(state.items.begin() + index);
   // Reduce the index of successive items.
   for (
-    vector<Item>::iterator i = items.begin() + index; i < items.end(); i++
+    vector<Item>::iterator i = state.items.begin() + index;
+    i < state.items.end();
+    i++
   ) {
     // TODO Could optimize further if we assume seeing items always in
     // TODO increasing order.
@@ -23,33 +63,56 @@ void Loader::handleDestroy(stringstream* tokens) {
   }
 }
 
-void Loader::handleItem(stringstream* tokens) {
-  Item item;
-  *tokens >> item.id;
-  if (item.id < 0) {
-    throw "negative item id";
+void Loader::handleExtent(stringstream& tokens) {
+  Item& item = getItem(tokens);
+  tokens >> item.extent(0);
+  tokens >> item.extent(1);
+}
+
+int Loader::handleId(stringstream& tokens) {
+  int id;
+  tokens >> id;
+  if (id < 0) {
+    throw "negative id";
   }
+  return id;
+}
+
+void Loader::handleItem(stringstream& tokens) {
+  Item item;
+  item.id = handleId(tokens);
+  // TODO Verify against duplicate ID?
   // TODO Evil data copy here. Do I care?
-  items.push_back(item);
+  state.items.push_back(item);
   while (indexes.size() < static_cast<size_t>(item.id + 1)) {
     // TODO Or can I make an allocator creating -1 by default for resize?
     indexes.push_back(-1);
   }
-  indexes[item.id] = items.size() - 1;
+  indexes[item.id] = state.items.size() - 1;
 }
 
-void Loader::handleType(stringstream* tokens) {
-  int id;
+void Loader::handleLocation(stringstream& tokens) {
+  Item& item = getItem(tokens);
+  tokens >> item.location(0);
+  tokens >> item.location(1);
+}
+
+void Loader::handleType(stringstream& tokens) {
+  Item& item = getItem(tokens);
   string type;
-  *tokens >> id;
-  *tokens >> type;
+  tokens >> type;
   // TODO Validate id.
-  Item& item = items[indexes[id]];
   if (type == "block") {
     item.type = stackiter::Block;
   } else if (type == "tool") {
     item.type = Tool;
   }
+}
+
+void Loader::handleVelocity(stringstream& tokens) {
+  Item& item = getItem(tokens);
+  tokens >> item.velocity(0);
+  tokens >> item.velocity(1);
 }
 
 void Loader::load(const string& name) {
@@ -63,16 +126,14 @@ void Loader::load(const string& name) {
   while (!getline(in, line).eof()) {
     stringstream tokens(line);
     tokens >> command;
-    // TODO Make a map of handlers or something.
-    if (command == "destroy") {
-      handleDestroy(&tokens);
-    } else if (command == "item") {
-      handleItem(&tokens);
-    } else if (command == "type") {
-      handleType(&tokens);
+    map<string,LoaderHandler>::iterator h = handlers.find(command);
+    if (h != handlers.end()) {
+      // We've defined a handler for this command, so handle it.
+      LoaderHandler handler = h->second;
+      (this->*handler)(tokens);
     }
   }
-  cout << "Items at end: " << items.size() << endl;
+  cout << "Items at end: " << state.items.size() << endl;
 }
 
 }
--- test/stackiter/stackiter-learner.h
+++ test/stackiter/stackiter-learner.h
@@ -2,8 +2,10 @@
 #define stackiter_learner_h
 
 #include <cuncuno.h>
+#include <Eigen/StdVector>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <sstream>
 #include <vector>
@@ -16,54 +18,138 @@ namespace stackiter {
 enum ItemType {
   Block,
   Tool,
+  Tray,
+  View,
+  World,
 };
 
 struct Item {
+
+  bool alive;
+
+  double angle;
+
+  double angularVelocity;
+
+  Vector3d color;
+
+  Vector2d extent;
+
+  /**
+   * Only ever true for blocks.
+   *
+   * This is somewhat cheating. Maybe ignore it.
+   */
+  bool grasped;
+
+  /**
+   * Only ever true for tools.
+   */
+  bool grasping;
+
   int id;
+
+  Vector2d location;
+
   ItemType type;
+
+  Vector2d velocity;
+
+  // To keep Eigen alignment happy.
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
 };
 
+}
+
+/**
+ * Have to define this outside namespace stackiter but before Item is used for
+ * vectors.
+ */
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(::stackiter::Item)
+
+// Back to your regularly scheduled namespace ...
+
+namespace stackiter {
+
 struct State {
-  MatrixXd angles;
-  MatrixXd angularVelocities;
-  MatrixXd colors;
-  MatrixXd extents;
-  MatrixXi ids;
-  MatrixXd locations;
-  Matrix<ItemType,Dynamic,Dynamic> types;
-  MatrixXd velocities;
+
+  vector<Item> items;
+
+  /**
+   * Simulation time, actually.
+   *
+   * TODO Break this out as a separately typed item?
+   */
+  double time;
+
 };
+
+// Fully arrayed version.
+// TODO How do I want to expose entity attributes to cuncuno?
+//struct State {
+//  MatrixXd angles;
+//  MatrixXd angularVelocities;
+//  MatrixXd colors;
+//  MatrixXd extents;
+//  MatrixXi ids;
+//  MatrixXd locations;
+//  Matrix<ItemType,Dynamic,Dynamic> types;
+//  MatrixXd velocities;
+//};
 
 // See: EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION
 // Also: http://forum.kde.org/viewtopic.php?f=74&t=82894#p174194
 
+struct Loader;
+typedef void(Loader::*LoaderHandler)(stringstream& tokens);
+
 struct Loader {
+
+  Loader();
 
   void load(const string& name);
 
 private:
 
-  void handleAngle(stringstream* tokens);
+  Item& getItem(stringstream& tokens);
 
-  void handleAngularVelocity(stringstream* tokens);
+  void handleAlive(stringstream& tokens);
 
-  void handleColor(stringstream* tokens);
+  void handleAngle(stringstream& tokens);
 
-  void handleDestroy(stringstream* tokens);
+  void handleAngularVelocity(stringstream& tokens);
 
-  void handleExtent(stringstream* tokens);
+  void handleColor(stringstream& tokens);
 
-  void handleItem(stringstream* tokens);
+  void handleDestroy(stringstream& tokens);
 
-  void handleLocation(stringstream* tokens);
+  void handleExtent(stringstream& tokens);
 
-  void handleType(stringstream* tokens);
+  void handleGrasp(stringstream& tokens);
 
-  void handleVelocity(stringstream* tokens);
+  static int handleId(stringstream& tokens);
+
+  void handleItem(stringstream& tokens);
+
+  void handleLocation(stringstream& tokens);
+
+  void handleRelease(stringstream& tokens);
+
+  void handleTime(stringstream& tokens);
+
+  void handleType(stringstream& tokens);
+
+  void handleVelocity(stringstream& tokens);
 
   vector<int> indexes;
 
-  vector<Item> items;
+  /**
+   * TODO Some way to make this static? Survive instance for now.
+   */
+  map<string, LoaderHandler> handlers;
+
+  State state;
 
 };
 
