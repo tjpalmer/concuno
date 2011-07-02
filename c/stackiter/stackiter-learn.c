8b5d9d54f4eee191f083fac392724c55a98cfb16
tjpalmer <tjpalmer@tjpalmer.com>
2011-07-02 11:41:03 -0500

    Extracted learn fall on.

--- c/stackiter/stackiter-learn.c
+++ c/stackiter/stackiter-learn.c
@@ -13,17 +13,17 @@ cnBool stInitSchemaAndEntityFunctions(
 );
 
 
+cnBool stLearnFallOn(
+  cnList(stState)* states, cnList(cnEntityFunction*)* functions
+);
+
+
 int main(int argc, char** argv) {
-  cnList(cnBag) bags;
-  cnLearner learner;
   cnList(cnEntityFunction*) entityFunctions;
-  cnRootNode* learnedTree;
   cnSchema schema;
   stState* state;
   cnList(stState) states;
   int status = EXIT_FAILURE;
-  cnRootNode stubTree;
-  cnCount trueCount;
 
   // Validate args.
   if (argc < 2) {
@@ -42,69 +42,23 @@ int main(int argc, char** argv) {
   printf("%ld items\n", state->items.count);
   printf("%ld states\n", states.count);
 
-  // Choose out the states we want to focus on.
-  cnListInit(&bags, sizeof(cnBag));
-  if (!stChooseDropWhereLandOnOther(&states, &bags)) {
-    printf("Failed to choose samples.\n");
-    goto DISPOSE_BAGS;
-  }
-  trueCount = 0;
-  cnListEachBegin(&bags, cnBag, bag) {
-    trueCount += bag->label;
-  } cnEnd;
-  // TODO Also print mean number of items in chosen states?
-  printf("%ld true of %ld samples\n", trueCount, bags.count);
-  // Shuffle bags, with controlled seed (and my own generator?).
-  // TODO Shuffle here copies more than just single pointers.
-  cnListShuffle(&bags);
-
   // Set up schema.
   if (!stInitSchemaAndEntityFunctions(&schema, &entityFunctions)) {
     printf("Failed to init schema and functions.\n");
-    goto DISPOSE_BAGS;
+    goto DISPOSE_STATES;
   }
 
-  // Set up the tree.
-  if (!cnRootNodeInit(&stubTree, cnTrue)) {
-    printf("Failed to init tree.\n");
+  // Attempt learning the "falls on" predictive concept.
+  if (!stLearnFallOn(&states, &entityFunctions)) {
+    printf("No learned tree.\n");
     goto DROP_FUNCTIONS;
   }
-  stubTree.entityFunctions = &entityFunctions;
 
-  // Propagate empty binding bags.
-  if (!cnRootNodePropagateBags(&stubTree, &bags)) {
-    printf("Failed to propagate bags in stub tree.\n");
-    goto DISPOSE_TREE;
-  }
-
-  // Learn a tree.
-  cnLearnerInit(&learner);
-  // TODO If no stored bindings, we'll need to pass them in here.
-  learnedTree = cnLearnTree(&learner, &stubTree);
-  if (!learnedTree) {
-    printf("No learned tree.\n");
-    goto DISPOSE_LEARNER;
-  }
-
+  // We winned it all!
   status = EXIT_SUCCESS;
-
-  // Dispose of the learned tree.
-  cnNodeDrop(&learnedTree->node);
-
-  DISPOSE_LEARNER:
-  cnLearnerDispose(&learner);
-
-  DISPOSE_TREE:
-  cnNodeDispose(&stubTree.node);
 
   DROP_FUNCTIONS:
   stDisposeSchemaAndEntityFunctions(&schema, &entityFunctions);
-
-  DISPOSE_BAGS:
-  cnListEachBegin(&bags, cnBag, bag) {
-    cnBagDispose(bag);
-  } cnEnd;
-  cnListDispose(&bags);
 
   DISPOSE_STATES:
   cnListEachBegin(&states, stState, state) {
@@ -194,4 +148,74 @@ cnBool stInitSchemaAndEntityFunctions(
   // Clean it all up if we fail.
   stDisposeSchemaAndEntityFunctions(schema, functions);
   return cnFalse;
+}
+
+
+cnBool stLearnFallOn(
+  cnList(stState)* states, cnList(cnEntityFunction*)* functions
+) {
+  cnList(cnBag) bags;
+  cnRootNode* learnedTree;
+  cnLearner learner;
+  cnBool result = cnFalse;
+  cnRootNode stubTree;
+  cnCount trueCount;
+
+  // Choose out the states we want to focus on.
+  cnListInit(&bags, sizeof(cnBag));
+  if (!stChooseDropWhereLandOnOther(states, &bags)) {
+    printf("Failed to choose samples.\n");
+    goto DISPOSE_BAGS;
+  }
+  trueCount = 0;
+  cnListEachBegin(&bags, cnBag, bag) {
+    trueCount += bag->label;
+  } cnEnd;
+  // TODO Also print mean number of items in chosen states?
+  printf("%ld true of %ld samples\n", trueCount, bags.count);
+  // Shuffle bags, with controlled seed (and my own generator?).
+  // TODO Shuffle here copies more than just single pointers.
+  cnListShuffle(&bags);
+
+  // Set up the tree.
+  if (!cnRootNodeInit(&stubTree, cnTrue)) {
+    printf("Failed to init tree.\n");
+    goto DISPOSE_BAGS;
+  }
+  stubTree.entityFunctions = functions;
+
+  // Propagate empty binding bags.
+  if (!cnRootNodePropagateBags(&stubTree, &bags)) {
+    printf("Failed to propagate bags in stub tree.\n");
+    goto DISPOSE_TREE;
+  }
+
+  // Learn a tree.
+  cnLearnerInit(&learner);
+  // TODO If no stored bindings, we'll need to pass them in here.
+  learnedTree = cnLearnTree(&learner, &stubTree);
+  if (!learnedTree) {
+    goto DISPOSE_LEARNER;
+  }
+
+  // We made it!
+  // TODO Any stats?
+  result = cnTrue;
+
+  // Dispose of the learned tree.
+  cnNodeDrop(&learnedTree->node);
+
+  DISPOSE_LEARNER:
+  cnLearnerDispose(&learner);
+
+  DISPOSE_TREE:
+  cnNodeDispose(&stubTree.node);
+
+  DISPOSE_BAGS:
+  cnListEachBegin(&bags, cnBag, bag) {
+    cnBagDispose(bag);
+  } cnEnd;
+  cnListDispose(&bags);
+
+  return result;
 }
