317eed8c01ad7e542285ce75a7b0d306b6663313
Tom Palmer <tjpalmer@tjpalmer.com>
2011-08-11 21:05:35 -0700

    More search setup.

--- c/concuno/search.c
+++ c/concuno/search.c
@@ -1,6 +1,96 @@
 #include "search.h"
+#include <stdio.h>
 
+
+typedef struct cnSearcherSelf {
+
+  /**
+   * The searcher is first.
+   */
+  struct cnSearcher searcher;
+
+  /**
+   * Prioritized options for search.
+   */
+  cnHeap(cnSearchOption) options;
+
+  // TODO Threading and mutexes?
+
+}* cnSearcherSelf;
+
+
+cnBool cnSearch(cnSearcher searcher) {
+  // TODO
+  return cnFalse;
+}
+
+
+void cnSearcherCreate_destroyItem(void* info, void* item) {
+  // Call destroy if we have one.
+  cnSearcher searcher = info;
+  if (searcher->destroyOption) searcher->destroyOption(searcher, item);
+}
+
+cnBool cnSearcherCreate_less(void* info, void* a, void* b) {
+  cnSearcher searcher = info;
+  // If it's better, it should bubble to the top.
+  return searcher->better(searcher, a, b);
+}
 
 cnSearcher cnSearcherCreate(void) {
-  return NULL;
+  cnSearcher searcher = NULL;
+  cnSearcherSelf self = malloc(sizeof(struct cnSearcherSelf));
+  if (!self) cnFailTo(DONE, "No searcher.");
+
+  // Safety first.
+  searcher = &self->searcher;
+  searcher->bestOption = NULL;
+  searcher->better = NULL;
+  searcher->destroyInfo = NULL;
+  searcher->destroyOption = NULL;
+  searcher->finished = NULL;
+  searcher->info = NULL;
+  cnListInit(&searcher->initialOptions, sizeof(cnSearchOption));
+  searcher->step = NULL;
+
+  // Now possible failures.
+  if (!(self->options = cnHeapCreate(cnSearcherCreate_less))) {
+    cnFailTo(FAIL, "No options heap.");
+  }
+  self->options->info = self;
+  self->options->destroyItem = cnSearcherCreate_destroyItem;
+
+  FAIL:
+  cnSearcherDestroy(searcher);
+  searcher = NULL;
+
+  DONE:
+  return searcher;
+}
+
+
+void cnSearcherDestroy(cnSearcher searcher) {
+  cnSearcherSelf self = (cnSearcherSelf)searcher;
+  if (!searcher) return;
+
+  // Options heap.
+  cnHeapDestroy(self->options);
+  self->options = NULL;
+
+  // Best and initial options.
+  // TODO Or is this a bad idea?
+  if (searcher->destroyOption) {
+    searcher->destroyOption(searcher, searcher->bestOption);
+    cnListEachBegin(&searcher->initialOptions, cnSearchOption, option) {
+      searcher->destroyOption(searcher, *option);
+    } cnEnd;
+  }
+
+  // Info.
+  if (searcher->destroyInfo) {
+    searcher->destroyInfo(searcher->info);
+  }
+
+  // And free.
+  free(self);
 }
--- c/concuno/search.h
+++ c/concuno/search.h
@@ -5,12 +5,104 @@
 #include "core.h"
 
 
-typedef struct cnSearcher {
-  //
-}* cnSearcher;
+/**
+ * A search option is a state in a forward graph search process. It's entirely
+ * abstract, managed by the client.
+ */
+typedef void* cnSearchOption;
+
+
+/**
+ * Implements forward graph search in nonmetric spaces by chasing higher
+ * priority ("better") options first.
+ *
+ * Actual searchers extend this structure, so don't presume this is everything.
+ *
+ * TODO Consider that RRT-inspired Monte Carlo search technique I saw at AAAI
+ * TODO 2011? Consider making algorithms pluggable at some point?
+ */
+typedef struct cnSearcher* cnSearcher;
+
+struct cnSearcher {
+
+  /**
+   * The best option found so far during the search process.
+   *
+   * TODO Destroyed with the searcher?
+   *
+   * TODO A list of bests? How to define?
+   */
+  cnSearchOption bestOption;
+
+  /**
+   * Compares options to know which to consider first. The option priorities
+   * should not change dynamically. Return true to say that a is better than b.
+   *
+   * This function must be thread safe. For example, the relevant data should
+   * be fixed before ever providing it to the searcher.
+   *
+   * TODO Actual bounds for B&B-type searches?
+   *
+   * TODO Support dynamic reordering? It would be expensive, and would need to
+   * TODO be explicit, but some needs might warrant it.
+   */
+  cnBool (*better)(cnSearcher searcher, cnSearchOption a, cnSearchOption b);
+
+  /**
+   * Set to non-null for automatic destruction of info.
+   *
+   * Destroy must ignore nulls.
+   */
+  void (*destroyInfo)(cnSearcher searcher);
+
+  /**
+   * Set to non-null for automatic destruction of options.
+   *
+   * Destroy must ignore nulls.
+   */
+  void (*destroyOption)(cnSearcher searcher, cnSearchOption option);
+
+  /**
+   * Says when the search is finished.
+   *
+   * If null, the search only terminates when all the options run out.
+   */
+  cnBool (*finished)(cnSearcher searcher);
+
+  /**
+   * Custom info for your own needs.
+   */
+  void *info;
+
+  /**
+   * The initial options from which to begin the search. At least one is needed.
+   */
+  cnList(cnSearchOption) initialOptions;
+
+  /**
+   * Given the current option, advance the search, providing the next options to
+   * consider.
+   *
+   * The step function must be thread safe when using threaded search.
+   */
+  cnBool (*step)(
+    cnSearcher searcher, cnSearchOption option, cnList(cnSearchOption)* nexts
+  );
+
+};
 
 
 cnSearcher cnSearcherCreate(void);
+
+
+void cnSearcherDestroy(cnSearcher searcher);
+
+
+/**
+ * Searches until finished. Returns true for no error. The best option is
+ * available inside the searcher.
+ */
+cnBool cnSearch(cnSearcher searcher);
 
 
 #endif
