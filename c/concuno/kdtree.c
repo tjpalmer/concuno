e5ce9d957fc80d10aed5f09e21cd304e4b93310d
Tom Palmer <tjpalmer@tjpalmer.com>
2011-08-23 17:30:06 -0500

    More thoughts on kd-trees. More yet to think.

--- c/concuno/kdtree.c
+++ c/concuno/kdtree.c
@@ -1,12 +1,34 @@
 #include "kdtree.h"
 
 
-cnKdSplitter cnKdSplitterCreate(cnRandom random) {
-  // TODO
+cnRootNode* cnKdSplit(cnKdSplitter splitter) {
   return NULL;
 }
 
 
+cnKdSplitter cnKdSplitterCreate(cnRandom random) {
+  cnKdSplitter splitter = malloc(sizeof(struct cnKdSplitter));
+  if (!splitter) cnFailTo(DONE, "No splitter.");
+  // Init.
+  splitter->pointMatrix = NULL;
+  splitter->random = random;
+  splitter->randomOwned = cnFalse;
+  // See if we need more.
+  if (!random) {
+    splitter->random = cnRandomCreate();
+    if (!splitter->random) {
+      free(splitter);
+      splitter = NULL;
+      cnFailTo(DONE, "No random.");
+    }
+    splitter->randomOwned = cnTrue;
+  }
+  DONE:
+  return splitter;
+}
+
+
 void cnKdSplitterDestroy(cnKdSplitter splitter) {
-  // TODO
+  if (splitter->randomOwned) cnRandomDestroy(splitter->random);
+  free(splitter);
 }
--- c/concuno/kdtree.h
+++ c/concuno/kdtree.h
@@ -9,11 +9,72 @@
 cnCBegin;
 
 
+/**
+ * Just a matrix of vectors, really.
+ *
+ * TODO Get grid working sometime?
+ *
+ * TODO Use this inside cnPointBag?
+ */
+typedef struct cnPointMatrix {
+
+  /**
+   * The total number of points in the bag.
+   */
+  cnCount pointCount;
+
+  /**
+   * An array of pointCount vectors of size valueCount each.
+   *
+   * TODO Arbitrary topologies?
+   */
+  cnFloat* points;
+
+  /**
+   * It's easy to imagine KD trees on angles, quaternions, categories, or other.
+   *
+   * TODO Support other than Euclidean.
+   */
+  cnTopology topology;
+
+  /**
+   * Number of homogeneous values per point.
+   */
+  cnCount valueCount;
+
+}* cnPointMatrix;
+
+
 typedef struct cnKdSplitter {
 
+  // TODO Modes such as random projection or PCA?
+  // TODO Or use different top level split functions for that?
+
+  /**
+   * An packed matrix of points to be split.
+   *
+   * TODO Arbitrary entities with functions (splittable on any)?
+   */
+  cnPointMatrix pointMatrix;
+
+  /**
+   * For maintaining random state.
+   */
   cnRandom random;
 
+  /**
+   * Whether the random is owned by the learner. Defaults to true when the
+   * random is created automatically for the learner.
+   *
+   * TODO Switch random to always destroyed and require others to null it before
+   * TODO destroying the splitter?
+   */
+  cnBool randomOwned;
+
 }* cnKdSplitter;
+
+
+cnRootNode* cnKdSplit(cnKdSplitter splitter);
 
 
 cnKdSplitter cnKdSplitterCreate(cnRandom random);
