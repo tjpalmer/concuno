b6bc8eb0f6a6e4c9aa7ee9c9a08519f5780eaec1
Tom Palmer <tjpalmer@tjpalmer.com>
2011-10-06 00:26:33 -0500

    Labeling holds vs. passes somewhat.

--- c/domains/robocup/choose.c
+++ c/domains/robocup/choose.c
@@ -8,17 +8,40 @@ cnBool cnrChoosePasses(
 
   // Loop through states.
   cnListEachBegin(&game->states, struct cnrState, state) {
-    // And players in those states to look for kicks.
+    // Look through players in each state to find kicks.
+    cnrPlayer kickingPlayer = NULL;
     cnListEachBegin(&state->players, struct cnrPlayer, player) {
-      if (!cnIsNaN(player->kickPower)) {
-        printf("Kick at %ld by %ld/%ld.\n", state->time, player->team, player->index);
+      if (player->team == cnrTeamKeepers && !cnIsNaN(player->kickPower)) {
+        if (kickingPlayer) {
+          cnFailTo(DONE, "Multiple kickers at %ld.", state->time);
+        }
+        kickingPlayer = player;
       }
     } cnEnd;
+
+    // Look ahead to see whether this kick was a pass or a hold.
+    if (kickingPlayer && state < end) {
+      // Ooh. We have a next state.
+      cnFloat* ballLocation = state->ball.item.location;
+      cnrState next = state + 1;
+      cnFloat* nextBallLocation = next->ball.item.location;
+      cnFloat ballVelocity[] = {
+        nextBallLocation[0] - ballLocation[0],
+        nextBallLocation[1] - ballLocation[1]
+      };
+      cnFloat ballSpeed = sqrt(
+        ballVelocity[0] * ballVelocity[0] + ballVelocity[1] * ballVelocity[1]);
+      cnFloat ballDistance = cnEuclideanDistance(2, nextBallLocation, kickingPlayer->item.location);
+      printf("%ld %ld %lg %lg %d\n", state->time, kickingPlayer->index, ballSpeed, ballDistance, ballSpeed <= 0.6 || ballDistance <= 0.6);
+    }
+
+    // Also look to see if the kick was "successful" in the sense that the
+    // keepers still have the ball.
   } cnEnd;
 
   // Winned.
   result = cnTrue;
 
-  // DONE:
+  DONE:
   return result;
 }
