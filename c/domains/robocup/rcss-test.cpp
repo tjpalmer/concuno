dddfbbbf25a767327f1d0912e5f8f90ead0fed3d
tjpalmer <tjpalmer@tjpalmer.com>
2011-10-27 12:30:59 -0500

    Some cpp-ifying.

--- c/domains/robocup/rcss-test.cpp
+++ c/domains/robocup/rcss-test.cpp
@@ -1,3 +1,5 @@
+#include <fstream>
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,13 +9,16 @@
 #include "load.h"
 
 
+using namespace std;
+
+
 cnBool cnrGenColumnVector(yajl_gen gen, cnCount count, cnFloat* x);
 
 
 cnBool cnrGenFloat(yajl_gen gen, cnFloat x);
 
 
-cnBool cnrGenStr(yajl_gen gen, char* str);
+cnBool cnrGenStr(yajl_gen gen, const char* str);
 
 
 /**
@@ -42,42 +47,46 @@ int main(int argc, char** argv) {
   char* name;
   int result = EXIT_FAILURE;
 
-  // Init first.
-  cnrGameInit(&game);
+  try {
+    // Init first.
+    cnrGameInit(&game);
 
-  // Check args.
-  if (argc < 2) cnErrTo(DONE, "No file specified.");
+    // Check args.
+    if (argc < 2) cnErrTo(DONE, "No file specified.");
 
-  // Load all the states in the game log.
-  // TODO Check if rcl or rcg to work either way.
-  name = argv[1];
-  if (!cnrLoadGameLog(&game, name)) cnErrTo(DONE, "Failed to load: %s", name);
+    // Load all the states in the game log.
+    // TODO Check if rcl or rcg to work either way.
+    name = argv[1];
+    if (!cnrLoadGameLog(&game, name)) cnErrTo(DONE, "Failed to load: %s", name);
 
-  // Show all team names.
-  cnListEachBegin(&game.teamNames, cnString, name) {
-    printf("Team: %s\n", cnStr(name));
-  } cnEnd;
+    // Show all team names.
+    cnListEachBegin(&game.teamNames, cnString, name) {
+      printf("Team: %s\n", cnStr(name));
+    } cnEnd;
 
-  // Now look at the command log to see what actions were taken.
-  // Assume the file is in the same place but named rcl instead of rcg.
-  if (cnStrEndsWith(name, ".rcg")) {
-    // Abusively just modify the arg. TODO Is this legit?
-    cnIndex lastIndex = strlen(name) - 1;
-    name[lastIndex] = 'l';
-    printf("Trying also to read %s\n", name);
-    if (!cnrLoadCommandLog(&game, name)) {
-      cnErrTo(DONE, "Failed to load: %s", name);
+    // Now look at the command log to see what actions were taken.
+    // Assume the file is in the same place but named rcl instead of rcg.
+    if (cnStrEndsWith(name, ".rcg")) {
+      // Abusively just modify the arg. TODO Is this legit?
+      cnIndex lastIndex = strlen(name) - 1;
+      name[lastIndex] = 'l';
+      printf("Trying also to read %s\n", name);
+      if (!cnrLoadCommandLog(&game, name)) {
+        cnErrTo(DONE, "Failed to load: %s", name);
+      }
     }
+
+    // Report.
+    printf("Loaded %ld states.\n", game.states.count);
+
+    // Extract actions of hold or kick to player, then process them.
+    if (!cnrProcess(&game, cnrProcessExport)) cnErrTo(DONE, "Failed extract.");
+
+    // Winned.
+    result = EXIT_SUCCESS;
+  } catch (const char* message) {
+    cout << message << endl;
   }
-
-  // Report.
-  printf("Loaded %ld states.\n", game.states.count);
-
-  // Extract actions of hold or kick to player, then process them.
-  if (!cnrProcess(&game, cnrProcessLearn)) cnErrTo(DONE, "Failed extract.");
-
-  // Winned.
-  result = EXIT_SUCCESS;
 
   DONE:
   cnrGameDispose(&game);
@@ -274,7 +283,7 @@ cnBool cnrProcessLearn(cnList(cnBag)* holdBags, cnList(cnBag)* passBags) {
 cnBool cnrSaveBags(const char* name, cnList(cnBag)* bags) {
   const unsigned char* buffer;
   size_t bufferSize;
-  FILE* file = NULL;
+  ofstream file;
   yajl_gen gen = NULL;
   const char* itemsKey = "items";
   const char* locationKey = "location";
@@ -282,7 +291,8 @@ cnBool cnrSaveBags(const char* name, cnList(cnBag)* bags) {
 
   printf("Writing %s\n", name);
   if (!(gen = yajl_gen_alloc(NULL))) cnErrTo(DONE, "No json formatter.");
-  if (!(file = fopen(name, "w"))) cnErrTo(DONE, "Failed to open: %s", name);
+  // TODO How to get custom info (like file names) on error message?
+  file.open(name); if (!file) throw "Failed to open output file.";
 
   if (!(
     yajl_gen_config(gen, yajl_gen_beautify, cnTrue) &&
@@ -345,15 +355,19 @@ cnBool cnrSaveBags(const char* name, cnList(cnBag)* bags) {
   // Output.
   // TODO Alternatively set the gen print function to print directly!
   if (yajl_gen_get_buf(gen, &buffer, &bufferSize)) cnFailTo(DONE);
-  if (fwrite(buffer, sizeof(char), bufferSize, file) < bufferSize) {
-    cnFailTo(DONE);
-  }
+  if (!file.write(
+    reinterpret_cast<const char*>(buffer),
+    static_cast<streamsize>(bufferSize)
+  )) throw "Failed to write to file.";
+
+  // Only bother with manual close if we didn't have other errors.
+  file.close();
+  if (!file) throw "Failed to close file.";
 
   // Winned.
   result = cnTrue;
 
   DONE:
-  if (file) fclose(file);
   if (gen) yajl_gen_free(gen);
   return result;
 }
